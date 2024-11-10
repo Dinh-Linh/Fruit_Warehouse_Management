7750f5b69cb7153287e9386534f0ba9c72bcd9ab
Dinh-Linh <linhnd26052004@gmail.com>
2024-11-10 15:46:56 +0700

    fix connect fe+be

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,12 @@ module com.example.demo {
     requires org.hibernate.orm.core;
     requires lombok;
 
+    opens entity to org.hibernate.orm.core;
+    opens generator to org.hibernate.orm.core;
+
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
     exports com.example.demo.entity;
     opens com.example.demo.entity to javafx.base;
+    exports dao;
 }
