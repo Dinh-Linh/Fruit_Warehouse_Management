1d55a12c4cb69394a555fd53f42c53c6b5e19a58
kdthinh <163316755+KieuDucThinh@users.noreply.github.com>
2024-11-22 09:13:03 +0700

    Delete src directory

--- src/main/java/com/example/demo/utils/Validator.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.utils;
-
-public class Validator {
-    public boolean isValidNumber(String input){
-        //Chỉ nhập số từ 1-9. Nếu có 1 số thì không được là số 0
-        return input.matches("^[1-9]$|^[0-9]{2,}$");
-    }
-}
